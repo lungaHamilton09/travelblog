6379199c5d672da945e1166733774f4617c4eb0a
lungaHamilton09 <141244743+lungaHamilton09@users.noreply.github.com>
2024-03-14 17:18:24 +0200

    Added more to the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import Navbar from './Components/Navbar/Navbar'
+import Home from './Components/Home/Home'
+import Search from './Components/Search/Search'
+import Support from './Components/Support/Support'
 
 const App = () => {
   return (
-    <div>Travel Blog</div>
+    <div>
+      <Travel Blog/>
+      <Home/>
+      <Search/>
+      <Support/>
+      <Travel Blog/>
+      <Travel Blog/>
+      <Travel Blog/>
+      </div>
   )
 }
 
